41cc3443b7af9d57fa8f72af58f43022300cc2bd
Mike Crifo <mike.crifo@gmail.com>
2022-01-11 16:37:30 -0500

    fixed es5 incompatibility with multiline in data js

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -6,14 +6,14 @@ export const homeObjOne = {
   topLine: "A Little Bit About Me..",
   headline: "Pleasure to virtually meet you!",
   description:
-    "My name is Mike Crifo, hailing out of Boston, Massachusetts, USA.  I am a newly trained\
-    full stack developer, with emphasis on Javascript, HTML and CSS.  Prior to coding, I was associated\
-    with the travel industry for over 19 years, in sales and management. \n\nFrom taking people around the globe \
-    through airlines and cruise ships, to now taking them virtually through their iPhones and laptops from one\
-    URL location to another, my goal is to create eye-catching and accessible user-friendly applications and websites\
-    alike! I'm motivated to continue learning new skills and techniques for better application with the right company.\
-    \n\n Please reach out to me using my contact me page (link on the button) or through any of my social media tabs listed on \
-    my left side bar!",
+    "My name is Mike Crifo, hailing out of Boston, Massachusetts, USA.  I am a newly trained " +
+    "full stack developer, with emphasis on Javascript, HTML and CSS.  Prior to coding, I was associated " +
+    "with the travel industry for over 19 years, in sales and management. \n\nFrom taking people around the globe " +
+    "through airlines and cruise ships, to now taking them virtually through their iPhones and laptops from one " +
+    "URL location to another, my goal is to create eye-catching and accessible user-friendly applications and websites " +
+    "alike! I'm motivated to continue learning new skills and techniques for better application with the right company. " +
+    "\n\n Please reach out to me using my contact me page (link on the button) or through any of my social media tabs listed on " +
+    "my left side bar!",
   buttonLabel: "Back to Top!",
   imgStart: true,
   img: require("../../images/smallme.png"),
@@ -48,10 +48,10 @@ export const homeObjThree = {
   topLine: "If You like what you See...",
   headline: "Please Reach Out to Connect!",
   description:
-    "Whether you have any questions for me, or something that you\
-    would like to create or improve, I'm ready to assist!\n\n Hit that 'Contact Me'\
-    Button on the top right or add me on any of the social links below and I will circle back\
-     as soon as I'm able!",
+    "Whether you have any questions for me, or something that you " +
+    "would like to create or improve, I'm ready to assist!\n\n Hit that 'Contact Me' " +
+    "Button on the top right or add me on any of the social links below and I will circle back " +
+     "as soon as I'm able!",
   buttonLabel: "Back to Top!",
   imgStart: true,
   img: require("../../images/DoctorComp.png"),
