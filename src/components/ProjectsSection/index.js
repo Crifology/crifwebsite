899493470d552400bcf4d4cfc0088659a6990bed
Mike Crifo <88783569+Crifology@users.noreply.github.com>
2024-03-06 15:13:04 -0500

    Update index.js

--- src/components/ProjectsSection/index.js
+++ src/components/ProjectsSection/index.js
@@ -11,18 +11,6 @@ export const Projects = () => {
             <ProjectsH1>My Projects!</ProjectsH1>
             <ProjectsWrapper>
 
-                /*
-                <ProjectsCard>
-                <ProjectIconsLink href="//crifcandycrush.herokuapp.com/" target="_blank" aria-label="CrifCandyCrush">
-                    <ProjectsIcon src={Icon1} />
-                </ProjectIconsLink>
-                <ProjectsH2>CrifCandyCrush - A CandyCrush Emulator</ProjectsH2>
-                <ProjectsP>This was my very first application that I used Javascript to create
-                It should be noted that minimal CSS and HTML was used, but was for the functionality
-                of the game rather than aestetics. </ProjectsP>
-                </ProjectsCard>
-                */
-
                 <ProjectsCard>
                 <ProjectIconsLink href="//pizzaface.crifology.com/" target="_blank" aria-label="PizzaFace!">
                 <ProjectsIcon src={Icon2} />
@@ -41,18 +29,6 @@ export const Projects = () => {
                 <ProjectsH2>ShopPlus+ Amazon Clone Creation</ProjectsH2>
                 <ProjectsP>My attempt at making a working eCommerce store using Login creations, order histories, and confirm purchases through backend. </ProjectsP>
                 </ProjectsCard>
-                
-
-                /*
-                <ProjectsCard>
-                <ProjectIconsLink href="//alphonsoscholarshipfund.s2.48in48.org/" target="_blank" aria-label="Covid 19 API">
-                <ProjectsIcon src={Icon4} />
-                </ProjectIconsLink>
-                <ProjectsH2>48in48 Project - Wordpress Website for Alphonso Scholarship Fund Inc</ProjectsH2>
-                <ProjectsP>Using Wordpress and working with 48in48's team of individuals, we constructed a complete multi-page website using 
-                Wordpress features within 48 hours.</ProjectsP>
-                </ProjectsCard>
-                */
         
             </ProjectsWrapper>
         </ProjectsContainer>
