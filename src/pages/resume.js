919663f0e25be8d48bef028d13c1ebb74c325b0b
Mike Crifo <88783569+Crifology@users.noreply.github.com>
2024-11-09 11:56:50 -0500

    Update resume.js

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -4,7 +4,7 @@ export const Resume = () => {
   return (
     <div>
       <iframe
-        src={"https://docs.google.com/document/d/1SnOd2XFZ5R57-4j9XHfK6e0uchLfEwpYYDJxUkS1HV8/edit?usp=sharing"}
+        src={"https://docs.google.com/document/d/e/2PACX-1vQK91TTlkDSZuYV739zAiJy9IzxqfSDvLN9q5z_2DSiRIdkXdw0VVkUCtAQJNv_wF8lh2fNHT-_Podd/pub"}
         title="file"
         width="100%"
         height="1300"
